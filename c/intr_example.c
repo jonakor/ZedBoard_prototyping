3083db232cbf2ffba5e073a8d73306ae881442d0
simennett <simennett@gmail.com>
2020-02-24 15:28:04 +0100

    fixed bug with interrupt startup. see issues on git

--- c/intr_example.c
+++ c/intr_example.c
@@ -23,9 +23,7 @@
 #define INTC_DEVICE_ID		XPAR_SCUGIC_SINGLE_DEVICE_ID
 #define GPIO_INTERRUPT_ID	XPAR_XGPIOPS_0_INTR
 
-#define INTR_TYPE         XGPIOPS_IRQ_TYPE_EDGE_RISING //Makes interrupt trig on rising edge. Defined in xgpiops.h 
-
-#define GPIO_BANK	XGPIOPS_BANK1  /* Bank 1 of the GPIO Device */
+#define INTR_TYPE         XGPIOPS_IRQ_TYPE_EDGE_BOTH //Makes interrupt trig on rising edge. Defined in xgpiops.h
 
 XGpioPs Gpio; /* The Instance of the GPIO Driver. Possibly static */
 XScuGic my_Gic; /* The Instance of the Interrupt Controller Driver. Possibly static */
@@ -71,29 +69,25 @@ int main(void) {
 
   Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,(Xil_ExceptionHandler)XScuGic_InterruptHandler, &my_Gic);
 
-  XScuGic_Connect(&my_Gic, GPIO_INTERRUPT_ID,(Xil_ExceptionHandler)my_intr_handler, (void *)&Gpio);
+  XScuGic_Connect(&my_Gic, GPIO_INTERRUPT_ID,(Xil_ExceptionHandler)my_intr_handler, (void *)&Gpio); //changed (void *)&Gpio to (void)
 
   XGpioPs_IntrClear(&Gpio, 0, 0x0); //clears out all interrupt enabled by fault
   XGpioPs_IntrClear(&Gpio, 1, 0x0);
   XGpioPs_IntrClear(&Gpio, 2, 0x0);
   XGpioPs_IntrClear(&Gpio, 3, 0x0);
-  
+
   /* Enable the GPIO interrupts on BPS and Flash signal pin */
   XGpioPs_IntrEnablePin(&Gpio, BPS_SIGNAL_PIN);
-  //XGpioPs_IntrEnablePin(&Gpio, FLASH_SIGNAL_PIN);
+  XGpioPs_IntrEnablePin(&Gpio, FLASH_SIGNAL_PIN);
   /* Enable falling edge interrupts for BPS and Flash signal pin*/
   XGpioPs_SetIntrTypePin(&Gpio, BPS_SIGNAL_PIN, INTR_TYPE);
-  //XGpioPs_SetIntrTypePin(&Gpio, FLASH_SIGNAL_PIN, INTR_TYPE);
+  XGpioPs_SetIntrTypePin(&Gpio, FLASH_SIGNAL_PIN, INTR_TYPE);
 
-  //XGpioPs_IntrClearPin(Gpio, BPS_SIGNAL_PIN); //possible fix to interrupt issue
-  //XGpioPs_IntrClearPin(Gpio, FLASH_SIGNAL_PIN);
+  XGpioPs_IntrClearPin(&Gpio, BPS_SIGNAL_PIN); //possible fix to interrupt issue
+  XGpioPs_IntrClearPin(&Gpio, FLASH_SIGNAL_PIN);
   XScuGic_Enable(&my_Gic, GPIO_INTERRUPT_ID);
-  //XGpioPs_IntrClearPin(Gpio, BPS_SIGNAL_PIN);
-  //XGpioPs_IntrClearPin(Gpio, FLASH_SIGNAL_PIN);
-  Xil_ExceptionEnableMask(XIL_EXCEPTION_IRQ);
-  //XGpioPs_IntrClearPin(Gpio, BPS_SIGNAL_PIN);
-  //XGpioPs_IntrClearPin(Gpio, FLASH_SIGNAL_PIN);
 
+  Xil_ExceptionEnableMask(XIL_EXCEPTION_IRQ);
 
   xil_printf("Interrupt Initialized\r\n");
 
@@ -112,34 +106,45 @@ int main(void) {
 
 static void my_intr_handler(void *CallBackRef){  //function called when interrupt occurs.
 
-  XGpioPs *Gpio = (XGpioPs *)CallBackRef;  //the same Gpio as outside interrupt handler.
-
   bool bpsSignal;
   bool flashSignal;
+  static bool prevBpsSignal = 0;
+  static bool prevFlashSignal = 0;
 
-  bpsSignal 	= XGpioPs_ReadPin(Gpio, BPS_SIGNAL_PIN); //read BPS signal to verify state
-  flashSignal = XGpioPs_ReadPin(Gpio, FLASH_SIGNAL_PIN); //read Flash signal to verify state
+  bpsSignal 	= XGpioPs_ReadPin(&Gpio, BPS_SIGNAL_PIN); //read BPS signal to verify state
+  flashSignal = XGpioPs_ReadPin(&Gpio, FLASH_SIGNAL_PIN); //read Flash signal to verify state
 
-  XGpioPs_WritePin(Gpio, LED_PIN, bpsSignal); // will be handy when interrupt triggers on both edges. led will blink according to BPS-signal
+  XGpioPs_WritePin(&Gpio, LED_PIN, bpsSignal); // will be handy when interrupt triggers on both edges. led will blink according to BPS-signal
 
   //BPS signal
-  if (bpsSignal == 1){
-    xil_printf("Rising edge of BPS!\r\n");
-    XGpioPs_IntrClearPin(Gpio, BPS_SIGNAL_PIN);
+  if (bpsSignal != prevBpsSignal) {
+    if (bpsSignal == 1){
+      xil_printf("Rising edge of BPS!\r\n");
+      XGpioPs_IntrClearPin(&Gpio, BPS_SIGNAL_PIN);
+    }
+    else if (bpsSignal == 0) {
+      xil_printf("Falling edge of BPS!\r\n");
+      XGpioPs_IntrClearPin(&Gpio, BPS_SIGNAL_PIN);
+    }
   }
-  else if (bpsSignal == 0) {
-    xil_printf("Falling edge of BPS!\r\n");
-    XGpioPs_IntrClearPin(Gpio, BPS_SIGNAL_PIN);
-  }
-  
+
+
+
+
   //Flash signal
-  if (flashSignal == 1){
-    xil_printf("Rising edge of Flash!\r\n");
-    XGpioPs_IntrClearPin(Gpio, FLASH_SIGNAL_PIN);
+  if (flashSignal != prevFlashSignal) {
+    if (flashSignal == 1){
+      xil_printf("Rising edge of Flash!\r\n");
+      XGpioPs_IntrClearPin(&Gpio, FLASH_SIGNAL_PIN);
+    }
+
+    else if (flashSignal == 0) {
+      xil_printf("Falling edge of Flash!\r\n");
+      XGpioPs_IntrClearPin(&Gpio, FLASH_SIGNAL_PIN);
+    }
   }
-  else if (flashSignal == 0) {
-    xil_printf("Falling edge of Flash!\r\n");
-    XGpioPs_IntrClearPin(Gpio, FLASH_SIGNAL_PIN);
-  }
+
+  prevBpsSignal = bpsSignal;
+  prevFlashSignal = flashSignal;
 
 }
