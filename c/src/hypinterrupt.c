9d2b5fd379e202a84009a320ccde9338a010ecf6
jonakor <jon18andreas@gmail.com>
2020-03-12 19:21:53 +0100

    began on interrupt source

--- c/src/hypinterrupt.c
+++ c/src/hypinterrupt.c
@@ -1,6 +1,8 @@
 /* INCLUDES */
+#include "linux/interrupt.h"
 
 /* DEFINES */
+#define IRQ_NUMBER 52
 
 /* FUNCTIONS */
 void hyp_interrupt_setup() {
@@ -8,11 +10,16 @@ void hyp_interrupt_setup() {
 }
 
 void hyp_interrupt_enable() {
-
+	request_irq(IRQ_NUMBER, timestamp_handler, 0, "timestamp", NULL);
 }
 
 void hyp_interrupt_disable() {
-
+	free_irq(IRQ_NUMBER, NULL);
 }
+
+static irqreturn_t timestamp_handler(int irq, void* dev_id) {
+	
+}
+
 
 /*STATIC FUNCTIONS*/
